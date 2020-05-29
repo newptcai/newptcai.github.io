6cb04ef5439355a1af3afeae10800a7cab316b4d
Xing Shi Cai <newptcai@gmail.com>
2020-05-29 19:49:14 +0200

    fix atom feed

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -14,6 +14,7 @@ from pelicanconf import *
 SITEURL = 'http://www2.math.uu.se/~xinca341'
 RELATIVE_URLS = False
 
+FEED_DOMAIN = SITEURL
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 #CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
